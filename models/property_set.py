67b8e6cf1ed5df22492ddcee0532513a0b48f7e1
dibang99 <dibang99@gmail.com>
2021-05-26 09:38:09 +0700

    update

--- models/property_set.py
+++ models/property_set.py
@@ -1,12 +1,14 @@
 from datetime import timedelta
 from odoo import models, fields, api, exceptions, _
 
+
 class PropertyGroup(models.Model):
     _name = 'rem.property.group'
     _description = "Set group for property"
 
     name = fields.Char(string="Group name", required=True)
     isActive = fields.Boolean(default='True')
+
 
 class PropertyType(models.Model):
     _name = 'rem.property.type'
@@ -15,14 +17,16 @@ class PropertyType(models.Model):
     name = fields.Char(string="Type name", required=True)
     isActive = fields.Boolean(default='True')
 
+
 class PropertyAddSet(models.Model):
     _name = 'product.template'
-    _inherit = 'product.template'    
+    _inherit = 'product.template'
 
-    sale_opening = fields.Selection([('opening','Opening'),('noopening','Noopening'),('sold','Sold')],'Set', default='noopening',help='')
+    sale_opening = fields.Selection([('opening', 'Opening'), ('noopening', 'Noopening'), (
+        'sold', 'Sold'), ('deposited', 'Deposited')], 'Set', default='noopening', help='')
 
-    group_name = fields.Many2one('rem.property.group',string='Group Name')
+    group_name = fields.Many2one('rem.property.group', string='Group Name')
     group_isActive = fields.Boolean('Group', related='group_name.isActive')
 
-    type_name = fields.Many2one('rem.property.type',string='Type Name')
+    type_name = fields.Many2one('rem.property.type', string='Type Name')
     type_isActive = fields.Boolean('Type', related='type_name.isActive')
