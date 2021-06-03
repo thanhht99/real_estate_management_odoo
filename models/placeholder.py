f16e50597c564aa1cf33b63dadd0b069ea439b21
Hoàng Tiến Thành <hoangtienthanh1999@gmail.com>
2021-06-03 22:47:04 +0700

    add contract

--- models/placeholder.py
+++ models/placeholder.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import logging
-from datetime import timedelta, time
+from datetime import timedelta,datetime
 
 from odoo import api, fields, models, _
 from odoo.addons.base.models.res_partner import WARNING_MESSAGE, WARNING_HELP
@@ -23,15 +23,18 @@ class Placeholder(models.Model):
     cart = fields.Many2one('rem.cart', string="Cart", required=True)
 
     price_holder = fields.Float(string='Cost to holder')
+    remaining_amount = fields.Float(string='Remaining amount', readonly=True)
 
     partner_id = fields.Many2one('res.partner', string="Customer" ,required=True)
+    seller_id = fields.Many2one('res.partner', string="Seller")
 
     payment_term_id = fields.Many2one('account.payment.term', string="Payment term")
 
     state = fields.Selection([
         ('draft', 'Draft'),
         ('paid', 'Paid'),
-        ('validate', 'Validate'),        
+        ('validate', 'Validate & Contract'),        
+        ('pay_the_rest', 'Pay the rest'),        
         ('done', 'Locked'),
         ('cancel', 'Cancelled'),
     ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', track_sequence=3, default='draft')
@@ -40,10 +43,10 @@ class Placeholder(models.Model):
         ('bank', 'Bank(VND)'),
         ('cash', 'Cash(VND)')
     ])
-    placeholder_date = fields.Date(string='Placeholder date')
+    placeholder_date = fields.Datetime(string='Placeholder date', default=datetime.now(), readonly=True)
     # product = fields.Many2one('product.product', required=True)
     payment_id = fields.Many2one('account.payment', string='Payment Name')
-    reservation_deadline = fields.Date(string='Reservation Deadline')
+    reservation_deadline = fields.Datetime(string='Reservation Deadline', readonly=True)
 
     placeholder_line = fields.One2many(
         'rem.placeholder.line', 'placeholder_id', string="Placeholder Lines", copy=True, auto_join=True)
@@ -53,6 +56,7 @@ class Placeholder(models.Model):
     def action_pay(self):
         self.ensure_one()
         self.state = 'paid'
+        self.reservation_deadline = self.placeholder_date + timedelta(days=7)
         payment = self.env['account.payment'].create({
             "name": self.cart.name + '/ ' + str(self.placeholder_line.product_id.name) + '/ ' + str(self.partner_id.name),
             "payment_type": 'inbound',
@@ -63,14 +67,20 @@ class Placeholder(models.Model):
             "journal_id": 7,
         })
         self.payment_id = payment
-        self.reservation_deadline = fields.Date.context_today
+        
 
     @api.multi
     def action_validate(self):
         self.ensure_one()
-        self.state = 'done'
+        self.state = 'validate'
         self.placeholder_line.mapped('product_id').write(
             {'sale_opening': 'done'})
+
+    @api.multi
+    def action_pay_the_rest(self):
+        self.ensure_one()
+        self.state = 'done'
+        self.remaining_amount = self.placeholder_line[0].price
 
 class PlaceholderLine(models.Model):
     _name = 'rem.placeholder.line'
